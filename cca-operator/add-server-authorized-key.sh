dd354712d6a4593690e57cc10d46044b067417d5
Adam Kariv <adam.kariv@gmail.com>
2018-11-15 14:18:02 +0200

    Set proper permissions to directories holding authorized_keys

--- cca-operator/add-server-authorized-key.sh
+++ cca-operator/add-server-authorized-key.sh
@@ -3,7 +3,7 @@
 [ "${1}" == "--help" ] && echo cat ~/.ssh/id_rsa.pub '|' ./add-server-authorized-key.sh && exit 0
 
 mkdir -p /root/.ssh &&\
-mkdir -p /etc/ckan-cloud/cca-operator &&\
+mkdir -p /etc/ckan-cloud/cca-operator && chmod 700 /etc/ckan-cloud && chmod 700 /etc/ckan-cloud/cca-operator && \
 cat >> /etc/ckan-cloud/cca-operator/sshd_authorized_keys
 [ "$?" != "0" ] && exit 1
 

e04c6a8c6851f62164003a565826226a2ec29bb1
Adam Kariv <adam.kariv@gmail.com>
2018-12-18 10:31:30 +0200

    Themer to save resulting css in local directory

--- ckan/themer/themer.sh
+++ ckan/themer/themer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cat <<EOF > /usr/lib/ckan/venv/src/ckan/ckan/public/base/less/custom.less
-@layoutLinkColor: $1;
+@layoutLinkColor: $CKAN_PRIMARY_COLOR;
 @footerTextColor: mix(#FFF, @layoutLinkColor, 60%);
 @footerLinkColor: @footerTextColor;
 @mastheadBackgroundColor: @layoutLinkColor;
 @btnPrimaryBackground: lighten(@layoutLinkColor, 10%);
 @btnPrimaryBackgroundHighlight: @layoutLinkColor;
 EOF
-lessc /usr/lib/ckan/venv/src/ckan/ckan/public/base/less/main.less
+lessc /usr/lib/ckan/venv/src/ckan/ckan/public/base/less/main.less /var/lib/ckan/main.css

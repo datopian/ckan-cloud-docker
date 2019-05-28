5f975f8f18f2b3eeb48d42af77d7533d60c336a8
ciolansteen <ciolansteen@gmail.com>
2019-05-28 11:21:38 +0300

    branch selection added to post_install_functions.sh

--- ckan/post_install_functions.sh
+++ ckan/post_install_functions.sh
@@ -1,13 +1,21 @@
 install_standard_ckan_extension_github() {
     REPO_NAME="${1}"
-    EGG="${2}"
+    BRANCH=""
+    # EGG is part of REPO_NAME
+    EGG=$(printf $REPO_NAME | cut -d / -f 2) 
+    # Use default value for branch if none specified
+    if [ $2 ]; then
+      BRANCH="@${2}"
+    fi
+    echo "############  Selected branch: $BRANCH #########"
     TEMPFILE=`mktemp`
     for REQUIREMENTS_FILE_NAME in requirements pip-requirements
     do
       if wget -O $TEMPFILE https://raw.githubusercontent.com/${REPO_NAME}/master/$REQUIREMENTS_FILE_NAME.txt
-      then ckan-pip install -r $TEMPFILE && break; fi
+        then ckan-pip install -r $TEMPFILE && break;
+      fi
     done &&\
-    ckan-pip install -e git+https://github.com/${REPO_NAME}.git#egg=${EGG} && break
+    ckan-pip install -e git+https://github.com/${REPO_NAME}.git$BRANCH#egg=${EGG} && break
 }
 
 install_bundled_requirements() {

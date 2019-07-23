764ed039d949cc2d3656b975e79211a5b1bab8a4
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-07-23 16:50:15 +0400

    remove instance id from datastore dbs

--- migrate_data.sh
+++ migrate_data.sh
@@ -8,6 +8,7 @@ DATASTORE_DB_BACKUP=${2}
 DB="${SQLALCHEMY_URL/:\/\/[^:]*:/:\/\/postgres:}"
 # Remove db name from URI to delete
 DB="${DB/\/ckan/}"
+DATASTORE_DB="${CKAN_DATASTORE_WRITE_URLB/\/datastore/}"
 
 echo $DB
 
@@ -24,9 +25,9 @@ fi
 
 # recover datastore DB
 if [ "$DATASTORE_DB_BACKUP" != "" ]; then
-  psql $DB -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'panama-datastore'"
-  psql $DB -c "DROP DATABASE panama-datastore;"
-  psql $DB -c "CREATE DATABASE panama-datastore;"
+  psql $DB -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'datastore'"
+  psql $DB -c "DROP DATABASE datastore;"
+  psql $DB -c "CREATE DATABASE datastore;"
   wget -O db_datastore_backup.gz $DATASTORE_DB_BACKUP && gunzip db_datastore_backup.gz
   psql $CKAN_DATASTORE_WRITE_URL -f db_datastore_backup ckan
 fi

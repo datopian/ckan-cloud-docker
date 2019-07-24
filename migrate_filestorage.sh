6b1592b343f7e97b56c41b9d2224b8079aaf351c
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-07-24 13:27:20 +0400

    script for migrating from s3 to file system

---
+++ migrate_filestorage.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Migrate the data from S3 FileStorage to local fyle system
+
+# FileStorage Server Eg: https://cc-p-minio.ckan.io or https://s3.amazonaws.com
+HOST=${1}
+ACCESS_KEY=${2}
+SECRET_KEY=${3}
+BUCKET=${4}
+STOREGE_PREFIX=${5}/
+TMP_DATA_RECIEVED='/tmp/data_recieved'
+TMP_DATA='/tmp/data'
+
+# Download minio client
+wget https://dl.min.io/client/mc/release/linux-amd64/mc chmod +x mc
+# Add host
+./mc config host add filestorage $HOST $ACCESS_KEY $SECRET_KEY
+echo downloading from storage
+./mc cp --recursive filestorage/$BUCKET/$STOREGE_PREFIX $TMP_DATA_RECIEVED
+
+echo updating paths
+for dir in $(ls $TMP_DATA_RECIEVED/resources); do
+  FIRST_DIR=${dir:0:3}
+  SECOND_DIR=${dir:3:3}
+  REST=${dir:6}
+  RESOURCE_DIR=$TMP_DATA/resources/$FIRST_DIR/$SECOND_DIR
+  mkdir -p $RESOURCE_DIR
+  for file in $TMP_DATA_RECIEVED/resources/$dir/*; do
+    echo $file
+    cp $file $RESOURCE_DIR/$REST
+  done
+done
+cp -r $TMP_DATA_RECIEVED/storage $TMP_DATA/storage
+
+echo mounting data into the persistant volumes
+docker cp $TMP_DATA ckanclouddocker_ckan_1:/var/lib/ckan

35dc046d6a85350416895857c79a41c6e0d28e40
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-07-24 13:52:41 +0400

    typo fixes

--- migrate_filestorage.sh
+++ migrate_filestorage.sh
@@ -7,8 +7,8 @@ HOST=${1}
 ACCESS_KEY=${2}
 SECRET_KEY=${3}
 BUCKET=${4}
-STOREGE_PREFIX=${5}/
-TMP_DATA_RECIEVED='/tmp/data_recieved'
+STORAGE_PREFIX=${5}/
+TMP_DATA_RECEIVED='/tmp/data_received'
 TMP_DATA='/tmp/data'
 
 # Download minio client
@@ -16,21 +16,21 @@ wget https://dl.min.io/client/mc/release/linux-amd64/mc chmod +x mc
 # Add host
 ./mc config host add filestorage $HOST $ACCESS_KEY $SECRET_KEY
 echo downloading from storage
-./mc cp --recursive filestorage/$BUCKET/$STOREGE_PREFIX $TMP_DATA_RECIEVED
+./mc cp --recursive filestorage/$BUCKET/$STORAGE_PREFIX $TMP_DATA_RECEIVED
 
 echo updating paths
-for dir in $(ls $TMP_DATA_RECIEVED/resources); do
+for dir in $(ls $TMP_DATA_RECEIVED/resources); do
   FIRST_DIR=${dir:0:3}
   SECOND_DIR=${dir:3:3}
   REST=${dir:6}
   RESOURCE_DIR=$TMP_DATA/resources/$FIRST_DIR/$SECOND_DIR
   mkdir -p $RESOURCE_DIR
-  for file in $TMP_DATA_RECIEVED/resources/$dir/*; do
+  for file in $TMP_DATA_RECEIVED/resources/$dir/*; do
     echo $file
     cp $file $RESOURCE_DIR/$REST
   done
 done
-cp -r $TMP_DATA_RECIEVED/storage $TMP_DATA/storage
+cp -r $TMP_DATA_RECEIVED/storage $TMP_DATA/storage
 
-echo mounting data into the persistant volumes
+echo mounting data into the persistent volumes
 docker cp $TMP_DATA ckanclouddocker_ckan_1:/var/lib/ckan

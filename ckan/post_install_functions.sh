518c8b9f3f9d58d082825654d55719267ca08b5b
zelima <ir.mchedlishvili@gmail.com>
2021-03-07 10:40:23 +0400

    Clone extensions if non standart servers and istall so

--- ckan/post_install_functions.sh
+++ ckan/post_install_functions.sh
@@ -8,6 +8,9 @@ install_standard_ckan_extension_github() {
 
     # By default, the master branch is used unless specified otherwise
     BRANCH="master"
+    GITHUB_URL=${GITHUB_URL:-https://github.com}
+    PIP_INDEX_URL=${PIP_INDEX_URL:-https://pypi.org/simple/}
+
     while getopts ":r:b:e:" options; do
       case ${options} in
         r) REPO_NAME=${OPTARG}
@@ -22,19 +25,32 @@ install_standard_ckan_extension_github() {
 #    echo "#### REPO: $REPO_NAME ####"
 #    echo "#### BRANCH: $BRANCH ####"
 #    echo "#### EGG: $EGG ####"
+    if [ $PIP_INDEX_URL != https://pypi.org/simple/ ]; then
+      TMPDIR='ckanext-temp'
+      git clone -b $BRANCH ${GITHUB_URL}/${REPO_NAME}.git ${TMPDIR}
 
-    TEMPFILE=`mktemp`
-    for REQUIREMENTS_FILE_NAME in requirements pip-requirements
-    do
-      if wget -O $TEMPFILE https://raw.githubusercontent.com/${REPO_NAME}/$BRANCH/$REQUIREMENTS_FILE_NAME.txt
-        then ckan-pip install --index-url ${PIP_INDEX_URL:-https://pypi.org/simple/} -r $TEMPFILE && break;
-      fi
-    done &&\
-    ckan-pip install --index-url ${PIP_INDEX_URL:-https://pypi.org/simple/} -e git+https://github.com/${REPO_NAME}.git@$BRANCH#egg=${EGG} && break
+      for REQUIREMENTS_FILE_NAME in requirements pip-requirements
+      do
+        if [ -f ${TMPDIR}/$REQUIREMENTS_FILE_NAME.txt ]; then
+          ckan-pip install --index-url ${PIP_INDEX_URL} -r ${TMPDIR}/$REQUIREMENTS_FILE_NAME.txt && break;
+        fi
+      done &&\
+      ckan-pip install --index-url ${PIP_INDEX_URL} -e ${TMPDIR}
+      rm -rf ${TMPDIR}
+    else
+      TEMPFILE=`mktemp`
+      for REQUIREMENTS_FILE_NAME in requirements pip-requirements
+      do
+        if wget -O $TEMPFILE https://raw.githubusercontent.com/${REPO_NAME}/$BRANCH/$REQUIREMENTS_FILE_NAME.txt
+          then ckan-pip install --index-url ${PIP_INDEX_URL} -r $TEMPFILE && break;
+        fi
+      done &&\
+      ckan-pip install --index-url ${PIP_INDEX_URL} -e git+${GITHUB_URL}/${REPO_NAME}.git@$BRANCH#egg=${EGG}
+    fi
 }
 
 install_bundled_requirements() {
-    ckan-pip install --index-url ${PIP_INDEX_URL:-https://pypi.org/simple/} -r "/tmp/${1}"
+    ckan-pip install --index-url ${PIP_INDEX_URL} -r "/tmp/${1}"
 }
 
 patch_ckan() {
